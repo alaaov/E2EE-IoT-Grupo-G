76db8cadcb1306667e308eea11dba40efda5badc
alaaov <38764468+alaaov@users.noreply.github.com>
2024-03-10 20:28:17 +0100

    send and recieve topic

--- app.py
+++ app.py
@@ -1,50 +1,282 @@
-import os
-from flask import Flask, render_template
+import paho.mqtt.client as mqtt
+from dotenv import load_dotenv
 from flask_socketio import SocketIO
 from argparse import ArgumentParser
-import paho.mqtt.client as mqtt
+from datetime import datetime, timedelta
+from flask import Flask, render_template, redirect
 
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+import os, bson, hmac, logging, threading, time
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from flask import Flask
+
+# parse arguments
 parser = ArgumentParser()
-parser.add_argument("-t", "--topics", dest="topics", nargs="+", required=True,
-                    help="the topics that you want to subscribe to")
+parser.add_argument("-t", "--topics", dest="topics", nargs="+",
+                    required=True, help="the topics that you want to subscribe to")
+parser.add_argument("-kea", "--key_exchange_algorithm", dest="key_exchange_algorithm",
+                    choices=["HADH", "ECDH"], default="HADH", help="the used algorithm for key exchange")
+parser.add_argument("-kg", "--key_generator", dest="key_generator", type=int, choices=[2, 5],
+                    default=2, help="g value for diffie hellman key generation")
+parser.add_argument("-ks", "--key_size", dest="key_size", type=int, choices=[512, 1024, 2048],
+                    default=512, help="key size for diffie hellman key generation")
+parser.add_argument("-kt", "--key_timeout", dest="key_timeout", type=int,
+                    default=30, help="the time after which we need to regenerate the encryption keys in seconds")
+
 args = parser.parse_args()
 
+load_dotenv()
+
 app = Flask(__name__)
+app.config['SECRET'] = 'my secret key'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+
+# Quitamos mensajes en la consola sobre GETs, POSTs y warnings
+logging.getLogger('socketio').setLevel(logging.ERROR)
+logging.getLogger('engineio').setLevel(logging.ERROR)
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
 socketio = SocketIO(app)
+# Diccionario con todos los dispositivos conectados y que han hecho intercambio Diffie-Hellman.
+# Contiene clave compartida (shared_key) y el nonce de la comunicación (nonce)
+devices = dict()
+unsub_devices = []
+# Diccionario que contiene claves público (pubkey) y privada (privkey) de la plataforma.
+platform_keys = dict()
 
-subscribed_topics = []  # Lista para almacenar los temas suscritos
+
+def RetationKey():
+    last_key_update = datetime.now()
+    while True:
+        update_time = datetime.now() - last_key_update
+        if update_time > timedelta(seconds=args.key_timeout):
+            last_key_update = datetime.now()
+            print("********************************* Regenerating keys *********************************")
+            start_DH()
+        else:
+            time.sleep(2)
 
 
-def on_message(client, userdata, message):
-    print(f"Received message: {message.payload.decode()} on topic {message.topic}")
+@app.route('/')
+def index():
+    return render_template('index.html', devices=devices)
 
 
-def config_Platform(client):
+@app.route('/unsubscribe/<string:sensor_type>/<string:device_id>', methods=['GET'])
+def unsubscribe(sensor_type, device_id):
+    print(f"security/{sensor_type}/{device_id}")
+    client.unsubscribe(f"security/{sensor_type}/{device_id}")
+    if device_id in devices:
+        devices.pop(device_id)
+    unsub_devices.append(device_id)
+    return redirect('/')
+
+
+def ChooseAlgorithm(algo, key, nonce):
+    algo = algo.split("/")
+    if algo[0] == 'ae':
+        case = algo[1]
+        if case == "aes":
+            return Cipher(algorithm=algorithms.AES256(key), mode=modes.GCM(nonce), backend=default_backend())
+        elif case == "camellia":
+            return Cipher(algorithm=algorithms.Camellia(key), mode=modes.CTR(nonce), backend=default_backend())
+        elif case == "chacha20":
+            return Cipher(algorithm=algorithms.ChaCha20(key, nonce), mode=None, backend=default_backend())
+        else:
+            return 'Authenticated encyption algorithm not found!'
+    elif algo[0] == 'aead':
+        case = algo[1]
+        if case == "aes":
+            return Cipher(algorithm=algorithms.AES256(key), mode=modes.GCM(nonce), backend=default_backend())
+        elif case == "chacha20":
+            return Cipher(algorithm=algorithms.ChaCha20(key, nonce), mode=None, backend=default_backend())
+        else:
+            return 'Authenticated encyption and additional data algorithm not found!'
+
+
+def register_device(message, client, device_id):
+    if device_id not in unsub_devices:
+        decoded_message = bson.loads(message)
+        serialized_sensor_public_key = decoded_message["pubkey"]
+        nonce = bytes.fromhex(decoded_message["nonce"])
+        algo = decoded_message["algorithm"].decode("utf-8")
+
+        shared_key = handle_sensor_public_key(serialized_sensor_public_key, device_id)
+
+        cipher = ChooseAlgorithm(algo, shared_key, nonce)
+        algo = algo.split("/")
+        devices[device_id] = {
+            "Device_id": device_id,
+            "sensor_type": decoded_message["sensor_type"].decode("utf-8"),
+            "shared_key": shared_key,
+            "nonce": nonce,
+            "cipher": cipher,
+            "encryption": algo
+        }
+
+        # generate signature hmac to compare with device's
+        signature = hmac.new(shared_key, (f"device id {device_id}").encode("UTF-8"), digestmod="sha256").digest()
+
+        # check if device is who it claims to be
+        try:
+            if signature == decoded_message["signature"]:
+                print(f"Sensor {device_id} succesfully authenticated")
+                print(f"Received new login from device {device_id}")
+                devices[device_id]["authenticated"] = True
+            else:
+                print("Invalid Authentication")
+                devices[device_id]["authenticated"] = False
+        except:
+            print("An exception occurred tryng to authenticate...")
+
+
+def handle_sensor_public_key(serialized_sensor_public_key, device_id):
+    # deserialize sensor public key
+    sensor_public_key = serialization.load_pem_public_key(serialized_sensor_public_key)
+    if args.key_exchange_algorithm == "HADH":
+        shared_secret = platform_keys["privkey"].exchange(sensor_public_key)
+    elif args.key_exchange_algorithm == "ECDH":
+        shared_secret = platform_keys["privkey"].exchange(ec.ECDH(), sensor_public_key)
+    else:
+        raise ValueError("Key exchange algorithm not supported")
+
+    hkdf = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b'',
+        backend=default_backend()
+    )
+    shared_key = hkdf.derive(shared_secret)
+    return shared_key
+
+
+def start_DH():
+    print("Generating keys...")
+
+    if args.key_exchange_algorithm == "HADH":
+        parameters = dh.generate_parameters(generator=args.key_generator, key_size=args.key_size)
+        private_key = parameters.generate_private_key()
+        public_key = private_key.public_key()
+        serialized_parameters = parameters.parameter_bytes(
+            serialization.Encoding.PEM,
+            serialization.ParameterFormat.PKCS3
+        )
+    elif args.key_exchange_algorithm == "ECDH":
+        # Generate an ephemeral private key for this exchange
+        private_key = ec.generate_private_key(ec.SECP256R1())
+        public_key = private_key.public_key()
+        serialized_parameters = None
+    else:
+        raise ValueError(f"Key exchange algorithm '{args.key_exchange_algorithm}' not supported")
+
+    # serialize public key
+    serialized_public_key = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
+    # encode data
+    encoded_data = {
+        "key_exchange_algorithm": args.key_exchange_algorithm,
+        "serialized_parameters": serialized_parameters,
+        "serialized_public_key": serialized_public_key
+    }
+    encoded_data = bson.dumps(encoded_data)
+
+    platform_keys["pubkey"] = public_key
+    platform_keys["privkey"] = private_key
+    # publish own public key to server
+    key_exchange_topic = "platform"
+    client.publish(key_exchange_topic, encoded_data, retain=True)
+    print(f"Public key published in topic {key_exchange_topic}")
+
+
+def start_platform_configuration():
     print("Subscribing to main topics")
     for topic in args.topics:
         t = topic
         if topic.lower() == "all":
             t = "*"
         client.subscribe(f"security/{t}/*")
-        client.subscribe(f"{t}/*")
-        client.subscribe(f"{t}/")
-        client.subscribe(f"{t}")
-        subscribed_topics.append(f"security/{t}/*")  # Agregar a la lista de temas suscritos
     client.subscribe("newDevice/*")
-
-    subscribed_topics.append("newDevice/*")  # Agregar a la lista de temas suscritos
-    # Establecer el manejador de mensajes
-    client.on_message = on_message
+    start_DH()
 
 
-def connect_broker(client, userdata, flags, rc):
+# On connect
+def handle_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Running on http://localhost:5000")
         print('Connected successfully')
-        config_Platform(client)
+        start_platform_configuration()
     else:
         print('Bad connection. Code:', rc)
 
+
+def decrypt_data(message, device_id):
+    key = devices[device_id]["shared_key"]
+    encription = devices[device_id]["encryption"]
+    cipher = devices[device_id]["cipher"]
+    algo = encription[1]
+    encription = encription[0]
+    decryptor = cipher.decryptor()
+    # Se comprueba si hay additional data y se verifica
+    if encription == "aead":
+        # Decrypt and verify the ciphertext and additional data
+        additional_data = hmac.new(key, device_id.encode("utf-8"), digestmod="sha256").digest()
+        decryptor.authenticate_additional_data(additional_data)
+
+    # Desencriptamos y obtenemos etiqueta de autenticación
+    if algo == "aes":
+        # Obtenemos etiqueta de autenticación del sensor
+        tag = message.payload[-16:]
+        # Desciframos y comprobamos etiqueta de autenticación
+        plaintext = decryptor.update(message.payload[:-16]) + decryptor.finalize_with_tag(tag)
+    else:
+        plaintext = decryptor.update(message.payload)
+    return plaintext.decode("utf-8")
+
+
+def identify_sensor(message):
+    if message == "humidity":
+        submit = "newHumidityData"
+    elif message == "temperature":
+        submit = "newTemperatureData"
+    elif message == "movement":
+        submit = 'newMovementData'
+    return submit
+
+
+# On message
+def handle_mqtt_message(client, userdata, message):
+    topic = message.topic.split("/")
+    if topic[0] == "newDevice":
+        device_id = topic[1]
+        register_device(message.payload, client, device_id)
+        return
+
+    # Identificamos el tipo de sensor y donde debe mandar los datos en la interfaz web
+    submit = identify_sensor(topic[1])
+    device_id = topic[2]
+    if device_id in devices and devices[device_id]["authenticated"] == True:
+        plaintext = decrypt_data(message, device_id)
+        devices[device_id]["last_value"] = plaintext
+        if plaintext != "NO":
+            print(f'({device_id}) Received new {submit[3:len(submit) - 4].lower()} value: {plaintext}')
+            data = dict(
+                topic=message.topic,
+                payload=plaintext
+            )
+            socketio.emit(submit, data=data)
+    else:
+        print(f"Sensor {device_id} not authenticated...")
+
+
+client = None
 
 if __name__ == '__main__':
     host = os.getenv("MQTT_BROKER_URL")
@@ -52,8 +284,13 @@ if __name__ == '__main__':
     username = os.getenv("MQTT_USERNAME")
     password = os.getenv("MQTT_PASSWORD")
     client = mqtt.Client(client_id="Platform")
-    client.username_pw_set("micalancer669", "tjeSCoiINVm89VAI")
-    client.on_connect = connect_broker
-    client.connect("micalancer669.cloud.shiftr.io", port, 60)
+    client.username_pw_set(username, password)
+    client.on_connect = handle_connect
+    client.connect(host, port, 60)
+    client.on_message = handle_mqtt_message
+    thread = threading.Thread(target=RetationKey)
+    thread.daemon = True
+    thread.start()
     client.loop_start()
     socketio.run(app, host='localhost', port=5000)
+
---
+++ device/device.py
@@ -0,0 +1,239 @@
+from dotenv import load_dotenv
+import paho.mqtt.client as mqtt
+from argparse import ArgumentParser
+import time, hmac, random, os, bson
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import string
+import secrets
+
+class Device:
+    def __init__(self, topic, output_function, args):
+        # Credenciales de conexión
+        load_dotenv()
+        self.host = os.getenv("MQTT_BROKER_URL")
+        self.port = 1883
+        self.username = os.getenv("MQTT_USERNAME")
+        self.password = os.getenv("MQTT_PASSWORD")
+        # Temas MQTT
+        self.topic = f"security/{topic}"
+        self.output_function = output_function
+        self.nonce = os.urandom(12)
+        self.stop_publish = False
+
+        if hasattr(args, 'encryption_algorithm'):
+            self.algorithm = f"ae/{args.encryption_algorithm.lower()}"
+            if args.encryption_algorithm.lower() == "camellia" or args.encryption_algorithm.lower() == "chacha20":
+                self.nonce = os.urandom(16)
+        elif hasattr(args, 'encryption_algorithm_additionasl_data'):
+            self.algorithm = f"aead/{args.encryption_algorithm_additional_data.lower()}"
+        self.type_sensor = args.topic
+
+        self.device_id = self.generate_deviceId()
+        self.client = self.connect()
+        self.key = None
+        self.private_key, self.public_key = None, None
+        self.other_public_key = None
+        self.connection_stablished = False
+
+    # Función de conexión con shiftr.io
+    def on_connect(self, client, userdata, flags, rc):
+        self.client.subscribe("platform")
+        if self.type_sensor == "humidity":
+            print("Conectado a shiftr.io con código de resultado: " + str(rc))
+
+    def generate_dh_keys(self, parameters):
+        private_key = parameters.generate_private_key()
+        public_key = private_key.public_key()
+        serialized_public_key = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        return private_key, public_key, serialized_public_key
+
+    def generate_ecdh_keys(self):
+        # Generate an ephemeral private key for this exchange
+        ephemeral_private_key = ec.generate_private_key(ec.SECP256R1())
+        ephemeral_public_key = ephemeral_private_key.public_key()
+        serialized_ephemeral_public_key = ephemeral_public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo)
+        return ephemeral_private_key, ephemeral_public_key, serialized_ephemeral_public_key
+
+    def diffie_hellman(self, client, payload):
+        # first receive the parameters and the other's public key
+        if self.type_sensor == "humidity":
+            print("Generating keys...")
+        decoded_message = bson.loads(payload)
+        # load algorithm and other's public key
+        key_exchange_algorithm = decoded_message["key_exchange_algorithm"]
+        serialized_other_public_key = decoded_message["serialized_public_key"]
+        # Extract the received public key from the message
+        other_public_key = serialization.load_pem_public_key(
+            serialized_other_public_key)
+        if key_exchange_algorithm == "HADH":
+            serialized_parameters = decoded_message["serialized_parameters"]
+            parameters = serialization.load_pem_parameters(serialized_parameters)
+            # generate own key pair
+            private_key, public_key, serialized_public_key = self.generate_dh_keys(parameters)
+            # get shared secret
+            shared_secret = private_key.exchange(other_public_key)
+        elif key_exchange_algorithm == "ECDH":
+            private_key, public_key, serialized_public_key = self.generate_ecdh_keys()
+            # Compute the shared secret using the received public key and the ephemeral private key
+            shared_secret = private_key.exchange(ec.ECDH(), other_public_key)
+        else:
+            raise ValueError(f"key exchange algorithm {key_exchange_algorithm} not supported.")
+
+        hkdf = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=None,
+            info=b'',
+            backend=default_backend()
+        )
+        self.key = hkdf.derive(shared_secret)
+
+        # generate signature hmac to authenticate device
+        signature = hmac.new(self.key, (f"device id {self.device_id}").encode("UTF-8"),
+                             digestmod="sha256").digest()
+
+        # publish own public key to server and signature
+        credentials = {
+            "pubkey": serialized_public_key,
+            "nonce": str(self.nonce.hex()),
+            "signature": signature,
+            "algorithm": self.algorithm.encode("utf-8"),
+            "sensor_type": self.type_sensor.encode("utf-8")
+        }
+        client.publish(f'newDevice/{self.device_id}', bson.dumps(credentials))
+        if self.type_sensor == "humidity":
+            print("Published own public key to platform")
+
+        # Ajustamos la longitud de la clave secreta para que cumpla los requisitos de longitud del algoritmo escogido
+        self.cipher = self.chooseEncryptionAlgorithm(self.key)
+
+    def on_message(self, client, userdata, message):
+        if message.topic == "platform":
+            self.stop_publish = True
+            self.diffie_hellman(
+                client,
+                message.payload
+            )
+            # Reanudamos comunicación una vez terminado el intercambio
+            self.stop_publish = False
+            return
+        else:
+            if self.type_sensor == "humidity":
+                print(message.topic)
+
+    def connect(self) -> mqtt.Client:
+        # Conexión con shiftr.io
+        client = mqtt.Client(client_id=f"{self.type_sensor}:{self.device_id}")
+        client.username_pw_set(self.username, self.password)
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
+        client.connect(self.host, self.port, 60)
+        return client
+
+    def generate_deviceId(self):
+        alphabet = string.ascii_letters + string.digits
+        return ''.join(secrets.choice(alphabet) for _ in range(10))
+
+    def encrypt_data(self, cipher, message, additional_data):
+        encription = self.algorithm.split("/")
+        encryptor = cipher.encryptor()
+        if encription[0] == "aead":
+            encryptor.authenticate_additional_data(additional_data)
+        ciphertext = encryptor.update(message.encode("utf-8"))
+        ciphertext += encryptor.finalize()
+        if encription[1] == "aes":
+            authentication_tag = encryptor.tag
+            self.client.publish(f"{self.topic}/{self.device_id}", ciphertext + authentication_tag)
+        else:
+            self.client.publish(f"{self.topic}/{self.device_id}", ciphertext)
+
+        # Publicar los valores en los temas MQTT correspondientes
+        if self.type_sensor == "humidity":
+            print(f"({self.device_id}) New {self.type_sensor} value: {message}")
+
+    def generateNewMessage(self):
+        message = str(self.output_function())
+        if self.type_sensor in ["humidity", "temperature"]:
+            message = message[0:4]
+        return message
+
+    def chooseEncryptionAlgorithm(self, key):
+        algo = self.algorithm.split("/")
+        if algo[0] == 'ae':
+            case = algo[1]
+            if case == "aes":
+                return Cipher(algorithm=algorithms.AES256(key), mode=modes.GCM(self.nonce), backend=default_backend())
+            elif case == "camellia":
+                return Cipher(algorithm=algorithms.Camellia(key), mode=modes.CTR(self.nonce), backend=default_backend())
+            elif case == "chacha20":
+                return Cipher(algorithm=algorithms.ChaCha20(key, self.nonce), mode=None, backend=default_backend())
+            else:
+                return 'Authenticated encyption algorithm not found!'
+        elif algo[0] == 'aead':
+            case = algo[1]
+            if case == "aes":
+                return Cipher(algorithm=algorithms.AES256(key), mode=modes.GCM(self.nonce), backend=default_backend())
+            elif case == "chacha20":
+                return Cipher(algorithm=algorithms.ChaCha20(key, self.nonce), mode=None, backend=default_backend())
+            else:
+                return 'Authenticated encyption and additional data algorithm not found!'
+
+    def run(self, args):
+        while self.key == None:
+            self.client.loop()
+            time.sleep(1)
+        if self.type_sensor == "humidity":
+            print("Conection stablished with platform")
+        self.topic = f"security/{self.type_sensor}"
+        # Generamos los datos adicionales que será la DeviceID
+        additional_data = hmac.new(self.key, self.device_id.encode("utf-8"), digestmod="sha256").digest()
+        # Bucle principal
+        while self.stop_publish == False:
+            self.client.loop()
+            message = self.generateNewMessage()
+            self.encrypt_data(self.cipher, message, additional_data)
+            # Esperar 5 segundos antes de la siguiente lectura
+            time.sleep(args.publish_timeout)
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-t", "--topic", dest="topic",
+                        required=True, help="the topic where we send the data. This decide the type of sensor")
+    parser.add_argument("-pt", "--publish_timeout", dest="publish_timeout", type=float,
+                        default=5, help="the time after which the sensor will send new data in seconds")
+    # TODO: NOT IMPLEMENTED YET
+    parser.add_argument("-ae", "--encryption_algorithm", dest="encryption_algorithm",
+                        choices=["aes", "camellia", "chacha20"], default="aes",
+                        help="the algorithm for authenticated encryption of messages")
+    parser.add_argument("-aead", "--encryption_algorithm_additional_data", dest="encryption_algorithm_additional_data",
+                        choices=["aes", "chacha20"], default="aes",
+                        help="the algorithm for authenticated encryption and additional data of messages")
+    parser.add_argument("-ot", "--output_type", dest="output_type",
+                        choices=["float", "boolean"], default="float", help="define the sensor's output type")
+    parser.add_argument("--min", dest="min", type=float,
+                        default=18, help="min output value", required=False)
+    parser.add_argument("--max", dest="max", type=float,
+                        default=25, help="max output value", required=False)
+    args = parser.parse_args()
+    if args.output_type == "float":
+        output_function = lambda: random.uniform(args.min, args.max)
+    elif args.output_type == "boolean":
+        output_function = lambda: random.choice([True, False])
+    else:
+        raise ValueError("output_type can not be this type")
+
+    sensor = Device(topic=args.topic, output_function=output_function, args=args)
+    sensor.run(args)
+
+
+if __name__ == "__main__":
+    main()
